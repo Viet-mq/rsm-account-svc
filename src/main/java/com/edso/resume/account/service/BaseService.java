0738266b1904fead2549a38dcfcc33d7cca9acbe
DESKTOP-88FAOV3\EdsoLabs <>
2022-01-04 15:48:06 +0700

    update search user

--- src/main/java/com/edso/resume/account/service/BaseService.java
+++ src/main/java/com/edso/resume/account/service/BaseService.java
@@ -6,7 +6,9 @@ import org.bson.conversions.Bson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.Normalizer;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public abstract class BaseService {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -19,5 +21,16 @@ public abstract class BaseService {
             return lst.get(0);
         }
         return Filters.and(lst);
+    }
+
+    public static String parseVietnameseToEnglish(String str) {
+        try {
+            String temp = Normalizer.normalize(str, Normalizer.Form.NFD);
+            Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+            return pattern.matcher(temp).replaceAll("").trim().toLowerCase().replaceAll("đ", "d");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "";
     }
 }
